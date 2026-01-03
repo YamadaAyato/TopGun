feec6c63a526c14580d7ba1be80cd8f0f7e92f16
ayato0913a <ayato0913a@gmail.com>
2026-01-03 22:46:58 +0900

    [ fix ] シーンをロードするクラスを作成

--- Assets/Scripts/System/SceneLoader.cs
+++ Assets/Scripts/System/SceneLoader.cs
@@ -1,16 +1,16 @@
-using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SceneLoader : MonoBehaviour
+/// <summary>
+///         SceneLoadをするクラス
+/// </summary>
+public static class SceneLoader
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    /// <summary>
+    ///         シーンをロードする
+    /// </summary>
+    /// <param name="sceneName">シーンの名前</param>
+    public static void LoadScene(string sceneName)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(sceneName);
     }
 }
