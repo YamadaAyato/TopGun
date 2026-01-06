27b760a891bd6c1ce90b6734f0bc20aa24167bed
ayato0913a <ayato0913a@gmail.com>
2026-01-06 12:07:41 +0900

    [ feat ] プレイヤーの状態を管理するクラスを作成

--- Assets/Scripts/Player/State/PlayerStateMachine.cs
+++ Assets/Scripts/Player/State/PlayerStateMachine.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
 
+/// <summary>　プレイヤーの状態を管理するクラス　</summary>
 public class PlayerStateMachine : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private IPlayerState _currentState;
+
+    /// <summary>　状態を変更する　</summary>
+    /// <param name="newState"></param>
+    public void ChangeState(IPlayerState newState)
     {
-        
+        _currentState?.OnExit();
+        _currentState = newState;
+        _currentState?.OnEnter();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        _currentState?.OnUpdate();
     }
 }
