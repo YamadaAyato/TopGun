23e5a070a4735b5bea7f8135e6c60af44763c787
ayato0913a <ayato0913a@gmail.com>
2026-01-06 11:56:48 +0900

    [ update ] Interfaceを定義

--- Assets/Scripts/Interface/IPlayerState.cs
+++ Assets/Scripts/Interface/IPlayerState.cs
@@ -1,16 +1,6 @@
-using UnityEngine;
-
-public class IPlayerState : MonoBehaviour
+public interface IPlayerState
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void OnEnter();
+    void OnUpdate();
+    void OnExit();
 }
