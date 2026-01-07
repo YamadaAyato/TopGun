ab8426fc578d125d0934d39e062358f8ae8ec8b6
ayato0913a <ayato0913a@gmail.com>
2026-01-07 13:38:39 +0900

    [ feat ] 最低速度を制限できるように

--- Assets/Scripts/Player/PlayerAirCraftController.cs
+++ Assets/Scripts/Player/PlayerAirCraftController.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerAirCraftController : MonoBehaviour
 {
+    [Header("前進移動の設定")]
     [SerializeField] private float _baseSpeed;
+    [SerializeField] private float _minSpeed;
     [SerializeField] private float _maxSpeed;
     [SerializeField] private float _acceleration;
     
@@ -13,10 +15,14 @@ public class PlayerAirCraftController : MonoBehaviour
     private Rigidbody _rb;
     private float _currentSpeed;
 
+    /// <summary>
+    ///         前進移動の処理をするクラス
+    ///         加速や減速の入力を受け取り、Rigidbodyの速度を更新する
+    /// </summary>
     private void FowardMovement()
     {
         _currentSpeed += _inputHandler.Throttle * _acceleration * Time.fixedDeltaTime;
-        _currentSpeed = Mathf.Clamp(_currentSpeed, 0f, _maxSpeed);
+        _currentSpeed = Mathf.Clamp(_currentSpeed, _minSpeed, _maxSpeed);
 
         Vector3 forwardMovement = transform.forward * _currentSpeed;
         _rb.linearVelocity = forwardMovement;
