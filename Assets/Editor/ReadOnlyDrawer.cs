1ec1583869b63749e8f21ad5d6268fde1f83e618
ayato0913a <ayato0913a@gmail.com>
2026-01-07 14:07:51 +0900

    [ feat ] Drawerを作成

--- Assets/Editor/ReadOnlyDrawer.cs
+++ Assets/Editor/ReadOnlyDrawer.cs
@@ -1,7 +1,14 @@
 using UnityEditor;
 using UnityEngine;
 
+/// <summary> ReadOnlyAttribute 用の Inspector 描画制御 </summary>
 [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
 public class ReadOnlyDrawer : PropertyDrawer
 {
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        GUI.enabled = false; // ここで編集不可に設定
+        EditorGUI.PropertyField(position, property, label);
+        GUI.enabled = true;　// ここで再び編集可能に設定
+    }
 }
