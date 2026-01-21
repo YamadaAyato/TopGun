a004b51d56a65e96745a6ee2ba28768e7c87ad16
ayato0913a <ayato0913a@gmail.com>
2026-01-21 18:49:45 +0900

    [ feat ] 敵の弾作成

--- Assets/Scripts/Bullet/EnemyBullet.cs
+++ Assets/Scripts/Bullet/EnemyBullet.cs
@@ -2,4 +2,24 @@ using UnityEngine;
 
 public class EnemyBullet : BulletBase
 {
+    [SerializeField] private int _damage;
+
+    protected override void HandleHit(Collider other)
+    {
+        if(other.TryGetComponent<PlayerHealth>(out PlayerHealth hit))
+        {
+            if (hit.CanBeHit)
+            {
+                hit.TakeDamage(_damage);
+            }
+
+            Release();
+            return;
+        }
+
+        if(other.CompareTag("Obstacle"))
+        {
+            Release();
+        }
+    }
 }
