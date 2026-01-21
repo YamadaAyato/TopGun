458343890aa69aa6013770330f7694180251782b
ayato0913a <ayato0913a@gmail.com>
2026-01-22 04:03:43 +0900

    [ feat ] ObjectPoolの機能追加

--- Assets/Scripts/System/ObjectPool.cs
+++ Assets/Scripts/System/ObjectPool.cs
@@ -3,5 +3,42 @@ using UnityEngine;
 
 public class ObjectPool<T> where T : MonoBehaviour
 {
+    private readonly Queue<T> _pool = new();
+    private readonly T _instance;
+    private readonly Transform _parent;
 
+    public ObjectPool(T instance, Transform parent, int initCount)
+    {
+        _instance = instance; 
+        _parent = parent;
+
+        for (int i = 0; i < initCount; i++)
+        {
+            Create();
+        }
+    }
+
+    public T Get()
+    {
+        if (_pool.Count == 0)
+            Create();
+
+        T obj = _pool.Dequeue();
+        obj.gameObject.SetActive(true);
+        return obj;
+    }
+
+    public void Release(T obj)
+    {
+        obj.gameObject.SetActive(false);
+        _pool.Enqueue(obj);
+    }
+
+    private T Create()
+    {
+        T obj = Object.Instantiate(_instance, _parent);
+        obj.gameObject.SetActive(false);
+        _pool.Enqueue(obj);
+        return obj;
+    }
 }
