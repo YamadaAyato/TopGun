60b16a141d28443a69cbf42492c7f4ad6958a238
ayato0913a <ayato0913a@gmail.com>
2026-01-21 17:45:54 +0900

    [ feat ] HP機能実装

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField, ReadOnly,Tooltip("現在無敵がどうか")] private bool _isInvincible;
+    [SerializeField, ReadOnly] private int _currentHealth;
+    [SerializeField] private int _health;
 
-    // Update is called once per frame
-    void Update()
+    public void TakeDamage(int damage)
     {
-        
+        _health -= damage;
+        Debug.Log($"プレイヤーに{damage}ダメージ、現在HP{_health}");
+
+        if(_health <= 0)
+        {
+            // TODO:死亡時処理
+            _health = 0;
+            Debug.Log("プレイヤー死亡");
+        }
     }
 }
