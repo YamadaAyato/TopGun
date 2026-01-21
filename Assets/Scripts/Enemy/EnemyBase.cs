5452e5e1946555fafd9d2f6928bcdfe15f3176b0
ayato0913a <ayato0913a@gmail.com>
2026-01-22 05:40:49 +0900

    [ update ] Summary追加とかAction追加

--- Assets/Scripts/Enemy/EnemyBase.cs
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
 using System;
 
+/// <summary>
+///     敵の基底クラス
+/// </summary>
 public abstract class EnemyBase : MonoBehaviour, IDamegeble
 {
     [SerializeField, ReadOnly] private int _currentHp;
     [SerializeField] private int _maxHp;
 
-    Action<EnemyBase> _onRelease;
+     private Action<EnemyBase> _onRelease;
 
+    /// <summary> スポーン時の初期化処理をする </summary>
+    /// <param name="onRelease"> どう戻すかの関数 </param>
     public void Spawn(Action<EnemyBase> onRelease)
     {
         _onRelease = onRelease;
@@ -24,11 +29,15 @@ public abstract class EnemyBase : MonoBehaviour, IDamegeble
         }
     }
 
-    public void Die()
+    public virtual void Die()
     {
         _currentHp = 0;
     }
 
+    /// <summary>
+    /// 自身が役目を終えたことを通知し、
+    /// 生成時に渡された解放コールバックを呼び出す。
+    /// </summary>
     protected void Release()
     {
         _onRelease?.Invoke(this);
