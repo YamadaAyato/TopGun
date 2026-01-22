5aeee16797f57754b7b5f6520de4828b8043044e
ayato0913a <ayato0913a@gmail.com>
2026-01-22 17:15:28 +0900

    [ feat ] 作業保存

--- Assets/Scripts/Enemy/TurretEnemy.cs
+++ Assets/Scripts/Enemy/TurretEnemy.cs
@@ -1,19 +1,51 @@
 using UnityEngine;
 
-public class TurretEnemy : MonoBehaviour
+public class TurretEnemy : EnemyBase
 {
-    [SerializeField] private Transform _target;
-    [SerializeField] private Transform _muzzle;
     [SerializeField] private EnemyBullet _bulletPrefab;
-
+    [SerializeField] private Transform _muzzle;
     [SerializeField] private float _fireInterval;
-    [SerializeField] private int _poolSize;
+    [SerializeField] private int _bulletPoolSize;
+
+    [SerializeField] private PlayerHealth _targetPlayer;
 
     private ObjectPool<EnemyBullet> _bulletPool;
     private float _timer;
 
+    protected override void OnSpawned()
+    {
+        base.OnSpawned();
+        _timer = 0f;
+    }
+
+    private void Fire()
+    {
+        EnemyBullet enemyBullet = _bulletPool.Get();
+        enemyBullet.transform.position = _muzzle.position;
+
+        Vector3 dir = (_targetPlayer.transform.position - this.transform.position).normalized;
+        enemyBullet.transform.rotation = Quaternion.LookRotation(dir,Vector3.up);
+
+        //enemyBullet.Spawn(ReturnBullet);
+    }
+
+    private void ReturnBullet(EnemyBullet enemyBullet)
+    {
+        _bulletPool.Release(enemyBullet);
+    }
+
+    private void Awake()
+    {
+        _bulletPool = new ObjectPool<EnemyBullet>(_bulletPrefab, this.transform, _bulletPoolSize);
+    }
+
     private void Update()
     {
-
+        _timer += Time.deltaTime;
+        if (_timer > _fireInterval)
+        {
+            _timer = 0f;
+            Fire();
+        }
     }
 }
