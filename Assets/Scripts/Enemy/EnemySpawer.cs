9782090b6996e15a57efbf48bd81817ee1bff679
ayato0913a <ayato0913a@gmail.com>
2026-01-24 20:00:05 +0900

    [ feat ] 試す用のスポナー

--- Assets/Scripts/Enemy/EnemySpawer.cs
+++ Assets/Scripts/Enemy/EnemySpawer.cs
@@ -2,5 +2,28 @@ using UnityEngine;
 
 public class EnemySpawer : MonoBehaviour
 {
+    [SerializeField] TurretEnemy _turretPrefab;
+    [SerializeField] Transform _exsampleTransform;
+    [SerializeField] private int _poolSize;
 
+    private ObjectPool<TurretEnemy> _pool;
+
+    public TurretEnemy SpawnTurret(Vector3 spawnPosition, Quaternion spawnRotation)
+    {
+        TurretEnemy turret = _pool.Get();
+        turret.transform.SetPositionAndRotation(spawnPosition, spawnRotation);
+
+        turret.Spawn(ReturnEnemy);
+        return turret;
+    }
+
+    private void ReturnEnemy(EnemyBase enemy)
+    {
+        _pool.Release((TurretEnemy)enemy);
+    }
+
+    private void Awake()
+    {
+        _pool = new ObjectPool<TurretEnemy>(_turretPrefab, _exsampleTransform, _poolSize);
+    }
 }
