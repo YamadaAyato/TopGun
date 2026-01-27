bb9b2a87fcad17e4f156c3dd107d2531a3963b5c
ayato0913a <ayato0913a@gmail.com>
2026-01-28 05:42:33 +0900

    [ feat ] カウンター用の管理クラス追加

--- Assets/Scripts/Player/Counter/CounterToken.cs
+++ Assets/Scripts/Player/Counter/CounterToken.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class CounterToken : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public int CurrentToken => _currentToken;
+
+    [SerializeField] private int _maxToken;
+    private int _currentToken;
+
+    public void AddToken(int amount)
     {
-        
+        _currentToken += amount;
+        if (_currentToken > _maxToken)
+        {
+            _currentToken = _maxToken;
+        }
+
+        Debug.Log($"Counter Token追加. Current: {_currentToken}/{_maxToken}");
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool UseToken(int amount)
     {
-        
+        if (_currentToken >= amount)
+        {
+            _currentToken -= amount;
+            Debug.Log($"Counter Token使用. Current: {_currentToken}/{_maxToken}");
+            return true;
+        }
+        Debug.Log("Counter Token不足で使用不可.");
+        return false;
     }
 }
