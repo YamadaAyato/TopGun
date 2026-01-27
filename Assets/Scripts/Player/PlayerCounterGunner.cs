aa5e96289a0706f16905701a54cf7d2c5ad5b29b
ayato0913a <ayato0913a@gmail.com>
2026-01-28 08:26:50 +0900

    [ feat ] カウンター用のガンナーを作成(Log確認のみ)

--- Assets/Scripts/Player/PlayerCounterGunner.cs
+++ Assets/Scripts/Player/PlayerCounterGunner.cs
@@ -2,15 +2,56 @@ using UnityEngine;
 
 public class PlayerCounterGunner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private TimeDilationController _timeDlicon;
+    [SerializeField] private Transform _muzzle;
+    [SerializeField] private int _counterCost;
+    [SerializeField] private float _counterColdown;
+
+    private CounterToken _counterToken;
+    private PlayerInputHandler _inputHandler;
+    private float _counterCooldownTimer;
+
+    private void OnFirePerformed()
     {
-        
+        if (_timeDlicon == null) return;
+        if(!_timeDlicon.IsPlaying) return;
+
+        if (_counterCooldownTimer > 0f) return;
+        TryCounterAttack();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void TryCounterAttack()
     {
-        
+        if (_counterToken.CurrentToken < _counterCost) return;
+        Debug.Log("カウンター攻撃発動!");
+        // カウンター攻撃処理
+        _counterToken.UseToken(_counterCost);
+
+        // クールダウンタイマーのリセット
+        _counterCooldownTimer = _counterColdown;
     }
-}
+
+    private void Awake()
+    {
+        _inputHandler = GetComponent<PlayerInputHandler>();
+        _counterToken = GetComponent<CounterToken>();
+    }
+
+    private void Update()
+    {
+        if (_counterCooldownTimer > 0f)
+        {
+            _counterCooldownTimer -= Time.deltaTime;
+        }
+    }
+
+    private void OnEnable()
+    {
+        _inputHandler.FirePerformed += OnFirePerformed;
+    }
+
+    private void OnDisable()
+    {
+        _inputHandler.FirePerformed -= OnFirePerformed;
+    }
+}   
