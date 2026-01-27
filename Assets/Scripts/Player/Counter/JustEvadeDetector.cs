d51d4ee8bff6401676b88c732760eb94654938d2
ayato0913a <ayato0913a@gmail.com>
2026-01-28 07:29:39 +0900

    [ fix ] サマリ追加

--- Assets/Scripts/Player/Counter/JustEvadeDetector.cs
+++ Assets/Scripts/Player/Counter/JustEvadeDetector.cs
@@ -1,12 +1,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+///     ジャスト回避用の近くの銃弾を探知し、保持するクラス
+/// </summary>
 public class JustEvadeDetector : MonoBehaviour
 {
     [SerializeField] private float _justDistance;
 
     private readonly HashSet<Transform> _bullets = new();
 
+    /// <summary>
+    ///     現在ジャスト距離に弾があるか
+    /// </summary>
+    /// <param name="playerPos"></param>
+    /// <returns></returns>
     public bool IsJustEvade(Vector3 playerPos)
     {
         foreach (var bullet in _bullets)
@@ -18,6 +26,8 @@ public class JustEvadeDetector : MonoBehaviour
         }
         return false;
     }
+
+    // ======================追加と削除======================
 
     private void OnTriggerEnter(Collider other)
     {
