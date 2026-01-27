046d84cb21d3fd2d83c4b819f6ca46fc861b56fc
ayato0913a <ayato0913a@gmail.com>
2026-01-27 17:23:16 +0900

    [ feat ] 回避の判定のための検知用のスクリプトを仮で作成

--- Assets/Scripts/Player/Counter/JustEvadeDetector.cs
+++ Assets/Scripts/Player/Counter/JustEvadeDetector.cs
@@ -1,16 +1,37 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class JustEvadeDetector : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float _justDistance;
+
+    private readonly HashSet<Transform> _bullets = new();
+
+    public bool IsJustEvade(Vector3 playerPos)
     {
-        
+        foreach (var bullet in _bullets)
+        {
+            if (Vector3.Distance(bullet.position, playerPos) <= _justDistance)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("EnemyBullet"))
+        {
+            _bullets.Add(other.transform);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("EnemyBullet"))
+        {
+            _bullets.Remove(other.transform);
+        }
     }
 }
