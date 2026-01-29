e4a3cc5d8c73709062156af741cd03b989fa74b4
ayato0913a <ayato0913a@gmail.com>
2026-01-29 15:53:10 +0900

    [ feat ] 現在のターゲットを持つ機能を追加

--- Assets/Scripts/Player/Counter/CounterTargetMemory.cs
+++ Assets/Scripts/Player/Counter/CounterTargetMemory.cs
@@ -2,4 +2,16 @@ using UnityEngine;
 
 public class CounterTargetMemory : MonoBehaviour
 {
+    public Transform CurrentTarget => _currentTarget;
+ 
+    private Transform _currentTarget;
+    public void SetBullet(BulletBase bullet)
+    {
+        _currentTarget = bullet.Shooter;
+    }
+
+    public void Clear()
+    {
+        _currentTarget = null;
+    }
 }
