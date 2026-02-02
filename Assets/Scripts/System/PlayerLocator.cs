de4d76c4305ef8f51d1c4872b2d6d728c7e19dea
ayato0913a <ayato0913a@gmail.com>
2026-02-03 06:50:24 +0900

    [ fix ] サマリ追加

--- Assets/Scripts/System/PlayerLocator.cs
+++ Assets/Scripts/System/PlayerLocator.cs
@@ -1,19 +1,31 @@
 using UnityEngine;
 
+/// <summary>
+///     プレイヤーの参照情報を保持するクラス
+/// </summary>
 public class PlayerLocator : MonoBehaviour
 {
     public static PlayerLocator Instance { get; private set; }
 
+    /// <summary> プレイヤーの体力情報 </summary>
     public PlayerHealth PlayerHealth { get; private set; }
 
+    /// <summary> プレイヤーのTransform情報 </summary>
     public Transform PlayerTransform { get; private set; }
 
+    /// <summary>
+    ///     プレイヤーを登録する
+    /// </summary>
+    /// <param name="playerHealth"></param>
     public void Register(PlayerHealth playerHealth)
     {
         PlayerHealth = playerHealth;
         PlayerTransform = playerHealth.transform;
     }
 
+    /// <summary>
+    ///     プレイヤーの登録を解除する
+    /// </summary>
     public void Unregister()
     {
         PlayerHealth = null;
