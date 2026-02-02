5dc33a83643b0a8ce312ce9bfe1a88bfcc576b88
ayato0913a <ayato0913a@gmail.com>
2026-02-03 06:26:35 +0900

    [ fix ] サマリ追加

--- Assets/Scripts/Player/CameraSwicher.cs
+++ Assets/Scripts/Player/CameraSwicher.cs
@@ -1,6 +1,9 @@
 using Unity.Cinemachine;
 using UnityEngine;
 
+/// <summary>
+///     カメラを前方視点と後方視点で切り替えるクラス
+/// </summary>
 public class CameraSwicher : MonoBehaviour
 {
     [SerializeField] private CinemachineCamera _frontCamera;
@@ -8,6 +11,10 @@ public class CameraSwicher : MonoBehaviour
 
     private PlayerInputHandler _inputHandler;
 
+    /// <summary>
+    ///     カメラを前方視点・後方視点で切り替える
+    /// </summary>
+    /// <param name="isRearView"> trueで後方カメラへ </param>
     private void ApplyRearView(bool isRearView)
     {
         _frontCamera.Priority = isRearView ? 0 : 10;
