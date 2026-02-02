88ebd97a6edabd9ccf27d3aec8ed5a554276fc06
ayato0913a <ayato0913a@gmail.com>
2026-02-03 05:56:27 +0900

    [ feat ] 後ろ向きカメラとメインのカメラを切り替える機能を追加

--- Assets/Scripts/Player/CameraSwicher.cs
+++ Assets/Scripts/Player/CameraSwicher.cs
@@ -1,16 +1,31 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class CameraSwicher : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private CinemachineCamera _frontCamera;
+    [SerializeField] private CinemachineCamera _rearCamera;
+
+    private PlayerInputHandler _inputHandler;
+
+    private void ApplyRearView(bool isRearView)
     {
-        
+        _frontCamera.Priority = isRearView ? 0 : 10;
+        _rearCamera.Priority = isRearView ? 10 : 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        _inputHandler = GetComponent<PlayerInputHandler>();
+    }
+
+    private void OnEnable()
+    {
+        _inputHandler.RearViewPerformed += ApplyRearView;
+    }
+
+    private void OnDisable()
+    {
+        _inputHandler.RearViewPerformed -= ApplyRearView;
     }
 }
