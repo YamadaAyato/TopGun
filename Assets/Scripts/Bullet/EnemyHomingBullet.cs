3a92cee4d6847fef8e26a34f624dc313f39e1ffc
ayato0913a <ayato0913a@gmail.com>
2026-02-04 06:25:22 +0900

    [ feat ] ダメージ処理だけか来た

--- Assets/Scripts/Bullet/EnemyHomingBullet.cs
+++ Assets/Scripts/Bullet/EnemyHomingBullet.cs
@@ -1,6 +1,21 @@
 using UnityEngine;
 
-public class EnemyHomingBullet : BulletBase
+public class EnemyHomingBullet : HomingBulletBase
 {
-    
+    protected override void HandleHit(Collider other)
+    {
+        if (other.transform == Shooter) return;
+
+        if (other.TryGetComponent<IDamageable>(out IDamageable hit))
+        {
+            hit.TakeDamage(_damage);
+            Release();
+            return;
+        }
+
+        if (other.CompareTag("Obstacle"))
+        {
+            Release();
+        }
+    }
 }
