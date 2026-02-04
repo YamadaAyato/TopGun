61d020f6e982b151f32f54b92ce0101707462d6d
ayato0913a <ayato0913a@gmail.com>
2026-02-04 19:48:56 +0900

    [ feat ] ホーミング型のタレットを作成

--- Assets/Scripts/Enemy/TurretHomingEnemy.cs
+++ Assets/Scripts/Enemy/TurretHomingEnemy.cs
@@ -2,20 +2,92 @@ using UnityEngine;
 
 public class TurretHomingEnemy : TurretEnemyBase
 {
+    [Header("ホーミングタレット設定")]
+    [SerializeField] private EnemyHomingBullet _bullet;
+    [SerializeField] private int _bulletPoolSize;
+
+    [SerializeField] private float _lockOnTime;
+    [SerializeField] private float _rotationSpeed;
+
+    private ObjectPool<EnemyHomingBullet> _bulletPool;
+    private bool _canStartLock;
+    private bool _isLocking;
+    private float _lockOnTimer;
+
+    protected override void OnSpawned()
+    {
+        base.OnSpawned();
+
+        _isLocking = false;
+        _lockOnTimer = 0f;
+    }
+
     protected override void FireAtPlayer(Transform player)
     {
-        throw new System.NotImplementedException();
+        EnemyHomingBullet bullet = _bulletPool.Get();
+
+        bullet.transform.SetPositionAndRotation(_muzzle.position, _muzzle.rotation);
+        bullet.Spawn(ReturnBullet, this.transform);
+        bullet.SetTarget(player, _muzzle.forward);
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Rotate(Transform target)
     {
-        
+        Vector3 dir = target.position - this.transform.position;
+        dir.y = 0;
+
+        Quaternion desired = Quaternion.LookRotation(dir.normalized, Vector3.up);
+        transform.rotation = Quaternion.RotateTowards(
+            transform.rotation,
+            desired,
+            _rotationSpeed * Time.deltaTime
+        );
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ReturnBullet(BulletBase bullet)
     {
-        
+        _bulletPool.Release((EnemyHomingBullet)bullet);
+    }
+
+    protected override void Update()
+    {
+        _timer += Time.deltaTime;
+        _canStartLock = _timer >= _fireInterval;
+
+        bool hasPlayer =
+            TryGetPlayer(out Transform player) &&
+            IsPlayerInRange(player) &&
+            IsPlayerInFov(player) &&
+            HasLineOfSight(player);
+
+        if (hasPlayer && _canStartLock)
+        {
+            if (!_isLocking)
+            {
+                _isLocking = true;
+                _lockOnTimer = 0f;
+            }
+
+            _lockOnTimer += Time.deltaTime;
+            Rotate(player);
+
+            if (_lockOnTimer >= _lockOnTime)
+            {
+                FireAtPlayer(player);
+                _timer = 0f;
+                _isLocking = false;
+                _lockOnTimer = 0f;
+            }
+        }
+        else
+        {
+            _isLocking = false;
+            _lockOnTimer = 0f;
+        }
+    }
+
+    private void Awake()
+    {
+        _bulletPool = new ObjectPool<EnemyHomingBullet>(_bullet, this.transform, _bulletPoolSize);
     }
 }
