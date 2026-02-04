111daa86ca6fc252763e1c156bd31e8a780bacc1
ayato0913a <ayato0913a@gmail.com>
2026-02-04 18:47:28 +0900

    [ add ] クラス作成

---
+++ Assets/Scripts/Enemy/TurretHomingEnemy.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class TurretHomingEnemy : TurretEnemyBase
+{
+    protected override void FireAtPlayer(Transform player)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
