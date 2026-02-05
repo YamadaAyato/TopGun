0d324d88ce8b1495ecbe9b251b33af2d2de6d617
ayato0913a <ayato0913a@gmail.com>
2026-02-05 11:01:31 +0900

    [ fix ] サマリ追加

--- Assets/Scripts/Interface/IFlareKillable.cs
+++ Assets/Scripts/Interface/IFlareKillable.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
 
+/// <summary>
+///     フレアによって倒されることができるオブジェクトのインターフェース
+/// </summary>
 public interface IFlareKillable
 {
+    /// <summary> フレアによって倒される </summary>
     void KillByFlare(Vector3 hitPoint);
 }
