4f020f0e677798eb241e95081025ee1c3f53f181
ayato0913a <ayato0913a@gmail.com>
2026-02-05 11:01:17 +0900

    [ fix ] サマリ追加

--- Assets/Scripts/Interface/IKillableBullet.cs
+++ Assets/Scripts/Interface/IKillableBullet.cs
@@ -1,6 +1,11 @@
 using UnityEngine;
 
+/// <summary>
+///     外部から消されることができる弾のインターフェース
+/// </summary>
 public interface IKillableBullet
 {
+    /// <summary> 弾を消す </summary>
+    /// <param name="hitPoint"> ヒット時などエフェクトを発生させる用のポジション </param>
     void Kill(Vector3 hitPoint);
 }
