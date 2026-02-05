e27a80b67ee12efb9d1a352033cf32384abdcc0d
ayato0913a <ayato0913a@gmail.com>
2026-02-06 00:08:37 +0900

    [ feat ] デコイとして検知させる用のコライダークラスを追加

--- Assets/Scripts/Player/Flare/BulletDetectingFlareColider.cs
+++ Assets/Scripts/Player/Flare/BulletDetectingFlareColider.cs
@@ -2,4 +2,26 @@ using UnityEngine;
 
 public class BulletDetectingFlareColider : MonoBehaviour
 {
+    private FlareDecoyRoot _flareDecoyRoot;
+
+    private void Awake()
+    {
+        _flareDecoyRoot = GetComponentInParent<FlareDecoyRoot>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<IDecoyAttractable>(out IDecoyAttractable attractable))
+        {
+            attractable.SetDecoyTarget(_flareDecoyRoot.transform);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.TryGetComponent<IDecoyAttractable>(out IDecoyAttractable attractable))
+        {
+            attractable.ClearDecoyTarget(_flareDecoyRoot.transform);
+        }
+    }
 }
