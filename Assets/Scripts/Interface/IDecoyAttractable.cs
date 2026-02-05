c92545fc538f7c986b4413b2a69c8993db06ce7a
ayato0913a <ayato0913a@gmail.com>
2026-02-05 11:01:25 +0900

    [ fix ] サマリ追加

--- Assets/Scripts/Interface/IDecoyAttractable.cs
+++ Assets/Scripts/Interface/IDecoyAttractable.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
 
+/// <summary>
+///     デコイによって引き寄せられることができるオブジェクトのインターフェース
+/// </summary>
 public interface IDecoyAttractable
 {
+    /// <summary> ターゲットをデコイに設定する </summary>
+    /// <param name="decoyTransform"></param>
     void SetDecoyTarget(Transform decoyTransform);
+    /// <summary> ターゲットをデコイから外す </summary>
+    /// <param name="decoyTransform"></param>
     void ClearDecoyTarget(Transform decoyTransform);
 }
