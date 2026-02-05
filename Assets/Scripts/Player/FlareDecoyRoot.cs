251df87019e568c226d288df16d7449a0c16fa1b
ayato0913a <ayato0913a@gmail.com>
2026-02-05 23:49:42 +0900

    [ feat ] 爆発で消すようなフレア機能追加

--- Assets/Scripts/Player/FlareDecoyRoot.cs
+++ Assets/Scripts/Player/FlareDecoyRoot.cs
@@ -1,6 +1,76 @@
 using UnityEngine;
 
 
-public class FlareDecoyRoot : MonoBehaviour
+public class FlareDecoyRoot : MonoBehaviour, IFlareKillable
 {
+    [Header("フレア設定")]
+    [SerializeField] private float _lifeTime = 5f;
+    [SerializeField] private float _blastRadius;
+
+    [Header("各レイヤー設定")]
+    [SerializeField] private LayerMask _bulletLayer;
+    [SerializeField] private LayerMask _flareLayer;
+
+    private float _timer;
+    private bool _detonated;
+
+    public void KillByFlare(Vector3 hitPoint)
+    {
+        if (_detonated) return;
+        _detonated = true;
+        Destroy(this.gameObject);
+    }
+
+    public void Detonate(Vector3 hitPoint, IKillableBullet bullet)
+    {
+        if (_detonated) return;
+        _detonated = true;
+
+        bullet?.Kill(hitPoint);
+
+        var bullets = Physics.OverlapSphere
+            (transform.position,
+            _blastRadius,
+            _bulletLayer,
+            QueryTriggerInteraction.Collide);
+
+        for (int i = 0; i < bullets.Length; i++)
+        {
+            if (bullets[i].TryGetComponent<IKillableBullet>(out var b))
+            {
+                b.Kill(transform.position);
+            }
+        }
+
+        var flareBullets = Physics.OverlapSphere
+            (transform.position,
+            _blastRadius,
+            _flareLayer,
+            QueryTriggerInteraction.Collide);
+
+        for (int i = 0; i < flareBullets.Length; i++)
+        {
+            if (flareBullets[i].TryGetComponent<IFlareKillable>(out var f))
+            {
+                f.KillByFlare(transform.position);
+            }
+        }
+
+        Destroy(this.gameObject);
+    }
+
+    private void Update()
+    {
+        _lifeTime += Time.deltaTime;
+        if (_timer >= _lifeTime)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _blastRadius);
+    }
 }
