2fb79c45b404d9a25c121cae854e744f338ab2b1
ayato0913a <ayato0913a@gmail.com>
2026-02-08 00:46:48 +0900

    [ add ] 名前変更

---
+++ Assets/Scripts/Bullet/EnemyStraightBullet.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+/// <summary>
+///     敵の銃弾処理をするクラス
+/// </summary>
+public class EnemyStraightBullet : BulletBase
+{
+    protected override void HandleHit(Collider other)
+    {
+        if(other.TryGetComponent<PlayerHealth>(out PlayerHealth hit))
+        {
+            if (hit.CanBeHit)
+            {
+                hit.TakeDamage(_damage);
+            }
+
+            Release();
+            return;
+        }
+
+        if(other.CompareTag("Obstacle"))
+        {
+            Release();
+        }
+    }
+}
