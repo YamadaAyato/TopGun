879b021282fefd1954d0c72a312712c778d40943
ayato0913a <ayato0913a@gmail.com>
2026-02-08 04:08:48 +0900

    [ fix ] サマリ追加

--- Assets/Scripts/System/ProjectileService.cs
+++ Assets/Scripts/System/ProjectileService.cs
@@ -10,16 +10,23 @@ public class ProjectileService : MonoBehaviour
     [Header("参照")]
     [SerializeField] private EnemyStraightBullet _straightBulletPrefab;
     [SerializeField] private EnemyHomingBullet _homingBulletPrefab;
-    [SerializeField] private Transform _straightParent;
-    [SerializeField] private Transform _homingParent;
+    [SerializeField, Tooltip("直線弾の親")] private Transform _straightParent;
+    [SerializeField, Tooltip("ホーミング弾の親")] private Transform _homingParent;
 
     [Header("プールサイズ設定")]
-    [SerializeField] private int _straightBulletPoolSize;
-    [SerializeField] private int _homingBulletPoolSize;
+    [SerializeField, Tooltip("直線弾のプールサイズ")] private int _straightBulletPoolSize;
+    [SerializeField, Tooltip("ホーミング弾のプールサイズ")] private int _homingBulletPoolSize;
 
     private ObjectPool<EnemyStraightBullet> _straightPool;
     private ObjectPool<EnemyHomingBullet> _homingPool;
 
+    /// <summary>
+    ///     直線弾をスポーンする
+    /// </summary>
+    /// <param name="muzzle"> 銃口 </param>
+    /// <param name="shooter"> 撃ち手 </param>
+    /// <param name="dir"> 方向 </param>
+    /// <returns></returns>
     public EnemyStraightBullet SpawnStraightBullet(Transform muzzle, Transform shooter, Vector3 dir)
     {
         EnemyStraightBullet bullet = _straightPool.Get();
@@ -29,12 +36,19 @@ public class ProjectileService : MonoBehaviour
         return bullet;
     }
 
+    /// <summary>
+    ///     ホーミング弾をスポーンする
+    /// </summary>
+    /// <param name="muzzle"> 銃口 </param>
+    /// <param name="shooter"> 撃ち手 </param>
+    /// <param name="dir"> ターゲット </param>
+    /// <returns></returns>
     public EnemyHomingBullet SpawnHomingBullet(Transform muzzle, Transform shooter, Transform target)
     {
         EnemyHomingBullet bullet = _homingPool.Get();
         bullet.transform.SetPositionAndRotation(muzzle.position, muzzle.rotation);
         bullet.Spawn(ReturnHomingBullet, shooter); ;
-        bullet.SetTarget(target,muzzle.forward);
+        bullet.SetTarget(target, muzzle.forward);
         return bullet;
     }
 
