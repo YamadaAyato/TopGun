97ee10918deff9fc745ccc004a9e1a9750ced24b
ayato0913a <ayato0913a@gmail.com>
2026-02-08 04:25:53 +0900

    [ fix ] サマリ追加

--- Assets/Scripts/Player/Flare/FlareDecoyRoot.cs
+++ Assets/Scripts/Player/Flare/FlareDecoyRoot.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 
-
+/// <summary>
+///     フレアデコイの根本クラス
+/// </summary>
 public class FlareDecoyRoot : MonoBehaviour, IFlareKillable
 {
     [Header("フレア設定")]
@@ -14,6 +16,10 @@ public class FlareDecoyRoot : MonoBehaviour, IFlareKillable
     private float _timer;
     private bool _detonated;
 
+    /// <summary>
+    ///     フレアを撃ち落とした時の処理
+    /// </summary>
+    /// <param name="hitPoint"></param>
     public void KillByFlare(Vector3 hitPoint)
     {
         if (_detonated) return;
@@ -21,20 +27,28 @@ public class FlareDecoyRoot : MonoBehaviour, IFlareKillable
         Destroy(this.gameObject);
     }
 
+    /// <summary>
+    ///     消去処理
+    /// </summary>
+    /// <param name="hitPoint"></param>
+    /// <param name="bullet"></param>
     public void Detonate(Vector3 hitPoint, IKillableBullet bullet)
     {
         if (_detonated) return;
         _detonated = true;
 
         Debug.Log("Flare Decoy Detonated");
+        // 最初に当たった弾を消す
         bullet?.Kill(hitPoint);
 
+        // 爆発範囲内の弾を消す
         var bullets = Physics.OverlapSphere
             (transform.position,
             _blastRadius,
             _bulletLayer,
             QueryTriggerInteraction.Collide);
 
+        // 消去処理
         for (int i = 0; i < bullets.Length; i++)
         {
             if (bullets[i].TryGetComponent<IKillableBullet>(out var b))
@@ -43,12 +57,14 @@ public class FlareDecoyRoot : MonoBehaviour, IFlareKillable
             }
         }
 
+        // 範囲内の他のフレアデコイを消す
         var flareBullets = Physics.OverlapSphere
             (transform.position,
             _blastRadius,
             _flareLayer,
             QueryTriggerInteraction.Collide);
 
+        // 消去処理
         for (int i = 0; i < flareBullets.Length; i++)
         {
             if (flareBullets[i].TryGetComponent<IFlareKillable>(out var f))
