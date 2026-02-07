3000139f741e391cdd1eb9757b66ef5b3af08547
ayato0913a <ayato0913a@gmail.com>
2026-02-08 03:43:44 +0900

    [ feat ] 敵の射撃を一つで完結するために汎用的に使えるクラスとして追加

--- Assets/Scripts/Enemy/EnemyShooter.cs
+++ Assets/Scripts/Enemy/EnemyShooter.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 
+/// <summary>
+///     敵の射撃タイプ
+/// </summary>
 public enum EnemyShooterType
 {
     Straight,
@@ -7,12 +10,62 @@ public enum EnemyShooterType
     Random
 }
 
+/// <summary>
+///     敵の射撃管理をするクラス
+/// </summary>
 public class EnemyShooter : MonoBehaviour, IEnemyShooter
 {
+    [Header("参照")]
+    [SerializeField] private Transform _muzzle;
 
+    [Header("射撃設定")]
+    [SerializeField] private EnemyShooterType _shooterType;
+    [SerializeField, Range(0f, 1f)] private float _homingRate;
 
+    /// <summary>
+    ///     発射!!!!!!!!!
+    /// </summary>
+    /// <param name="shooter"></param>
+    /// <param name="target"></param>
     public void Fire(Transform shooter, Transform target)
     {
-        throw new System.NotImplementedException();
+        // nullチェック
+        if(_muzzle == null || shooter == null || target == null)
+        {
+            Debug.LogWarning("EnemyShooter: Muzzle, Shooter, or Target is null.");
+            return;
+        }
+        if(ProjectileService.Instance == null)
+        {
+            Debug.LogWarning("EnemyShooter: ProjectileService instance is null.");
+            return;
+        }
+
+        // 射撃タイプによって使い分けする
+        bool useHoming =_shooterType switch
+        {
+            EnemyShooterType.Straight => false,
+            EnemyShooterType.Homing => true,
+            _ => Random.value < _homingRate,
+        };
+
+        // 射撃へつなぐ
+        if(useHoming)
+        {
+            ProjectileService.Instance.SpawnHomingBullet(_muzzle, shooter, target);
+        }
+        else
+        {
+            Vector3 dir = (target.position - _muzzle.position).normalized;
+            ProjectileService.Instance.SpawnStraightBullet(_muzzle, shooter, dir);
+        }
+    }
+
+    public void FireStraight(Transform shooter, Vector3 dir)
+    {
+        if (_muzzle == null || shooter == null) return;
+        if (ProjectileService.Instance == null) return;
+
+        ProjectileService.Instance.SpawnStraightBullet(_muzzle, shooter, dir);
     }
 }
