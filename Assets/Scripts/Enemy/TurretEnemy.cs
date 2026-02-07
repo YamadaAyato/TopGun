f3e9a3a1951004d063d2969a589b5fb71e3ea02d
ayato0913a <ayato0913a@gmail.com>
2026-02-08 03:49:22 +0900

    [ fix ] いらねぇ処理消すぞ～～～

--- Assets/Scripts/Enemy/TurretEnemy.cs
+++ Assets/Scripts/Enemy/TurretEnemy.cs
@@ -5,12 +5,6 @@ using UnityEngine;
 /// </summary>
 public class TurretEnemy : TurretEnemyBase
 {
-    [Header("単発タレット設定")]
-    [SerializeField] private EnemyStraightBullet _bulletPrefab;
-    [SerializeField] private int _bulletPoolSize;
-
-    private ObjectPool<EnemyStraightBullet> _bulletPool;
-
     protected override void OnSpawned()
     {
         base.OnSpawned();
@@ -18,24 +12,6 @@ public class TurretEnemy : TurretEnemyBase
 
     protected override void FireAtPlayer(Transform player)
     {
-        // 銃弾をプールから取得して、銃口に
-        EnemyStraightBullet enemyBullet = _bulletPool.Get();
-        enemyBullet.transform.position = _muzzle.position;
-
-        // 方向を定めて銃弾を回転させる
-        Vector3 dir = (player.transform.position - _muzzle.position).normalized;
-        enemyBullet.transform.rotation = Quaternion.LookRotation(dir, Vector3.up);
-
-        enemyBullet.Spawn(ReturnBullet, this.transform);
-    }
-
-    private void ReturnBullet(BulletBase enemyBullet)
-    {
-        _bulletPool.Release((EnemyStraightBullet)enemyBullet);
-    }
-
-    private void Awake()
-    {
-        _bulletPool = new ObjectPool<EnemyStraightBullet>(_bulletPrefab, this.transform, _bulletPoolSize);
+        _shooter?.Fire(this.transform, player);
     }
 }
