9083a6e04bd6879e30fe84f237da7981edd00cfe
ayato0913a <ayato0913a@gmail.com>
2026-02-10 22:27:31 +0900

    [ feat ] 爆発エフェクトの単体クラス作った

--- Assets/Scripts/Effects/ExplosionFx.cs
+++ Assets/Scripts/Effects/ExplosionFx.cs
@@ -1,16 +1,37 @@
+using System;
 using UnityEngine;
 
 public class ExplosionFx : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private ParticleSystem _ps;
+    [SerializeField] private float _duration;
+
+    private Action<ExplosionFx> _onRelease;
+    private float _timer;
+
+    public void Play(Action<ExplosionFx> onRelease)
     {
-        
+        _onRelease = onRelease;
+        _timer = 0f;
+
+        if(_ps != null)
+        {
+            _ps.Clear();
+            _ps.Play();
+        }
+
+        gameObject.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        _timer += Time.deltaTime;
+        if(_timer >= _duration)
+        {
+            _onRelease?.Invoke(this);
+            gameObject.SetActive(false);
+
+            _onRelease = null;
+        }
     }
 }
