34d42c8d045be0bbfa8cea514898cc19b534b052
ayato0913a <ayato0913a@gmail.com>
2026-02-11 01:17:59 +0900

    [ fix ] 爆発タイプの追加と適用

--- Assets/Scripts/Effects/ExplosionFx.cs
+++ Assets/Scripts/Effects/ExplosionFx.cs
@@ -1,20 +1,36 @@
 using System;
 using UnityEngine;
 
+/// <summary>
+///     爆発エフェクトの種類
+/// </summary>
+public enum ExplosionType
+{
+    Small,
+    Big
+}
+
+/// <summary>
+///     爆発エフェクトの管理を行うクラス
+/// </summary>
 public class ExplosionFx : MonoBehaviour
 {
-    [SerializeField] private ParticleSystem _ps;
     [SerializeField] private float _duration;
 
     private Action<ExplosionFx> _onRelease;
     private float _timer;
+    private ParticleSystem _ps;
 
+    /// <summary>
+    ///     エフェクトを再生する
+    /// </summary>
+    /// <param name="onRelease"></param>
     public void Play(Action<ExplosionFx> onRelease)
     {
         _onRelease = onRelease;
         _timer = 0f;
 
-        if(_ps != null)
+        if (_ps != null)
         {
             _ps.Clear();
             _ps.Play();
@@ -23,10 +39,17 @@ public class ExplosionFx : MonoBehaviour
         gameObject.SetActive(true);
     }
 
+    private void Awake()
+    {
+        _ps = GetComponent<ParticleSystem>();
+    }
+
     private void Update()
     {
         _timer += Time.deltaTime;
-        if(_timer >= _duration)
+
+        // 再生時間を超えたらプールに返す
+        if (_timer >= _duration)
         {
             _onRelease?.Invoke(this);
             gameObject.SetActive(false);
