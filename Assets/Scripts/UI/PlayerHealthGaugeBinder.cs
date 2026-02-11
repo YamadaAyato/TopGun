ab23423c8a0a2ab597de8b71450c5c55c0ec38ce
ayato0913a <ayato0913a@gmail.com>
2026-02-11 10:18:20 +0900

    [ fix ] サマリとコメント追加

--- Assets/Scripts/UI/PlayerHealthGaugeBinder.cs
+++ Assets/Scripts/UI/PlayerHealthGaugeBinder.cs
@@ -1,11 +1,19 @@
 using UnityEngine;
 
+/// <summary>
+///     HPゲージとプレイヤーの体力情報を紐づけるクラス
+/// </summary>
 public class PlayerHealthGaugeBinder : MonoBehaviour
 {
     [SerializeField] private RadialGaugeView _gaugeView;
 
     private PlayerHealth _playerHealth;
 
+    /// <summary>
+    ///     ゲージ更新
+    /// </summary>
+    /// <param name="current"></param>
+    /// <param name="max"></param>
     private void UpdateGauge(int current, int max)
     {
         _gaugeView.SetNormalized((float)current / max);
@@ -13,6 +21,7 @@ public class PlayerHealthGaugeBinder : MonoBehaviour
 
     private void Start()
     {
+        // AwakeでPlayerLocatorが初期化されるのでここでとってくる
         if (_playerHealth == null)
         {
             _playerHealth = PlayerLocator.Instance.PlayerHealth;
