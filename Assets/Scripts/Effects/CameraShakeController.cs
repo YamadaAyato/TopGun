babdce42dba2a274cf91798793fe6a15f374a529
ayato0913a <ayato0913a@gmail.com>
2026-02-12 02:43:51 +0900

    [ feat ] CameraShake用の機能実装

--- Assets/Scripts/Effects/CameraShakeController.cs
+++ Assets/Scripts/Effects/CameraShakeController.cs
@@ -1,16 +1,48 @@
+using System.Xml.Serialization;
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class CameraShakeController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("参照")]
+    [SerializeField] private Camera _mainCamera;
+
+    [SerializeField] private float _hitGain;
+    [SerializeField] private float _explosionGain;
+    [SerializeField] private float _maxDistance;
+
+    [SerializeField] private AnimationCurve _fallOffCurve;
+    private CinemachineImpulseSource _impulseSource;
+
+    public void PlayHit(float intensity)
     {
-        
+        float gain = _hitGain * Mathf.Clamp01(intensity);
+        _impulseSource.GenerateImpulse(Vector3.up * gain);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayExplosion(Vector3 explosionPosition)
     {
-        
+        float distance = Vector3.Distance(_mainCamera.transform.position, explosionPosition);
+        float t = Mathf.Clamp01(distance / _maxDistance);
+        float fallOff = _fallOffCurve.Evaluate(t);
+        float gain = _explosionGain * fallOff;
+        _impulseSource.GenerateImpulse(Vector3.up * gain);
+    }
+
+    private void Awake()
+    {
+        _impulseSource = GetComponent<CinemachineImpulseSource>();
+    }
+
+    private void OnEnable()
+    {
+        GameEvents.OnPlayerHit += PlayHit;
+        GameEvents.OnExplosion += PlayExplosion;
+    }
+
+    private void OnDisable()
+    {
+        GameEvents.OnPlayerHit -= PlayHit;
+        GameEvents.OnExplosion -= PlayExplosion;
     }
 }
