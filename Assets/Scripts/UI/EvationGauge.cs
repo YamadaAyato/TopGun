bec231f60f5e8b8a233e0c22b8f51e7b58e29800
ayato0913a <ayato0913a@gmail.com>
2026-02-11 10:18:47 +0900

    [ fix ] サマリとコメント追加

--- Assets/Scripts/UI/EvationGauge.cs
+++ Assets/Scripts/UI/EvationGauge.cs
@@ -1,21 +1,30 @@
 using System;
 using UnityEngine;
 
+/// <summary>
+///     回避ゲージを管理するクラス
+/// </summary>
 public class EvationGauge : MonoBehaviour
 {
+    /// <summary> 回避のチャージ数が変化したときに発火するイベント / </summary>
     public event Action<float, int> OnChargesChanged;
 
-    public int MaxCharges => _maxCharges;
+    /// <summary> 現在のチャージ数  </summary>
     public float CurrentCharges => _currentCharges;
+    /// <summary> 最大チャージ数 </summary>
+    public int MaxCharges => _maxCharges;
     public bool IsEvading => _isEvading;
 
-    [SerializeField] private int _maxCharges;
-
-    [SerializeField] private float _regenChargesPerSecond;
     [SerializeField, ReadOnly] private float _currentCharges;
+    [SerializeField, Tooltip("最大チャージ数")] private int _maxCharges;
+    [SerializeField, Tooltip("1秒あたり何回分回復するか")] private float _regenChargesPerSecond;
 
     private bool _isEvading;
 
+    /// <summary>
+    ///     回避できるかどうかを返し、できるならチャージを1消費する
+    /// </summary>
+    /// <returns></returns>
     public bool TryConsumeCharge()
     {
         if (_currentCharges > 0)
@@ -27,6 +36,11 @@ public class EvationGauge : MonoBehaviour
         return false;
     }
 
+    /// <summary>
+    ///     チャージを回復する
+    ///     ジャスト回避時などに呼ばれる
+    /// </summary>
+    /// <param name="amount"></param>
     public void RecorverCharge(int amount)
     {
         _currentCharges = Mathf.Min(_currentCharges + amount, _maxCharges);
@@ -55,6 +69,7 @@ public class EvationGauge : MonoBehaviour
 
     private void Update()
     {
+        // 回避中でなく、最大チャージ数に達していなければ回復する
         if (_currentCharges < _maxCharges && !_isEvading)
         {
             _currentCharges += _regenChargesPerSecond * Time.deltaTime;
