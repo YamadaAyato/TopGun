4d79782c595318f2e1c10a6993b171e252bc5ddb
ayato0913a <ayato0913a@gmail.com>
2026-02-11 10:18:04 +0900

    [ fix ] サマリとコメント追加

--- Assets/Scripts/UI/ThresholdLinesView.cs
+++ Assets/Scripts/UI/ThresholdLinesView.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
 
+/// <summary>
+///     閾値を示す線分を管理するクラス
+/// </summary>
 public class ThresholdLinesView : MonoBehaviour
 {
     [SerializeField] private RectTransform _line1;
     [SerializeField] private RectTransform _line2;
     [SerializeField] private RectTransform _line3;
 
-    [SerializeField] private float _startAngleDeg;
+    [SerializeField, Tooltip("Filledの開始角に合わせる補正")] private float _startAngleDeg;
 
+    /// <summary>
+    ///     Lineをセットアップする
+    /// </summary>
+    /// <param name="maxCharges"></param>
     public void Setup(int maxCharges)
     {
         SetLine(_line1, 1f / maxCharges);
