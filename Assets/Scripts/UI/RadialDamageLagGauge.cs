99cfce8f30842bdbcd3444da2d8eeff5bb818d7e
ayato0913a <ayato0913a@gmail.com>
2026-02-11 15:33:33 +0900

    [ feat ] ダメージ演出用のスクリプト追加

--- Assets/Scripts/UI/RadialDamageLagGauge.cs
+++ Assets/Scripts/UI/RadialDamageLagGauge.cs
@@ -1,16 +1,41 @@
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RadialDamageLagGauge : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Image _mainFillImage;
+    [SerializeField] private Image _lagFillImage;
+
+    [SerializeField] private float _lagDelay;
+    [SerializeField] private float _catchUpSpeed;
+
+    private Tween _damageTween;
+
+    public void SetNormalized(float value)
     {
-        
+        // メインゲージを即座に更新
+        _mainFillImage.fillAmount = Mathf.Clamp01(value);
+        // ラグゲージの更新をキャンセル
+        _damageTween?.Kill();
+        // ラグゲージを遅延後に追従させる
+        float startValue = _lagFillImage.fillAmount;
+        float endValue = Mathf.Clamp01(value);
+        _damageTween = DOTween.To(
+            () => _lagFillImage.fillAmount,
+            x => _lagFillImage.fillAmount = x,
+            endValue,
+            _catchUpSpeed
+            )
+            .SetDelay(_lagDelay)
+            .SetEase(Ease.OutCubic)
+            .SetUpdate(true);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnDisable()
     {
-        
+        _damageTween?.Kill();
+        _damageTween = null;
     }
 }
