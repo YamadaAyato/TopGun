333325d71984b89334d754b09160d2cd0f8d660b
ayato0913a <ayato0913a@gmail.com>
2026-02-11 11:49:25 +0900

    [ fix ] サマリ追加

--- Assets/Scripts/UI/RadialGaugeView.cs
+++ Assets/Scripts/UI/RadialGaugeView.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+///     円ゲージの表示を管理するクラス
+/// </summary>
 public class RadialGaugeView : MonoBehaviour
 {
     [SerializeField] private Image _fillImage;
     [SerializeField] private Image _centerIconImage;
 
+    /// <summary>
+    ///     0〜1の範囲でゲージを設定する
+    /// </summary>
+    /// <param name="value"></param>
     public void SetNormalized(float value)
     {
         _fillImage.fillAmount = Mathf.Clamp01(value);
     }
 
+    /// <summary>
+    ///     アイコンを設定する
+    /// </summary>
+    /// <param name="icon"></param>
     public void SetCenterIcon(Sprite icon)
     {
         _centerIconImage.sprite = icon;
