6eaa4e35d2d1d5080fe56f20324a6e70e369015b
ayato0913a <ayato0913a@gmail.com>
2026-02-11 09:58:45 +0900

    [ feat ] 回避ができる閾値を分割して表示するクラスを追加

--- Assets/Scripts/UI/ThresholdLinesView.cs
+++ Assets/Scripts/UI/ThresholdLinesView.cs
@@ -2,15 +2,22 @@ using UnityEngine;
 
 public class ThresholdLinesView : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private RectTransform _line1;
+    [SerializeField] private RectTransform _line2;
+    [SerializeField] private RectTransform _line3;
+
+    [SerializeField] private float _startAngleDeg;
+
+    public void Setup(int maxCharges)
     {
-        
+        SetLine(_line1, 1f / maxCharges);
+        SetLine(_line2, 2f / maxCharges);
+        SetLine(_line3, 3f / maxCharges);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetLine(RectTransform line, float t)
     {
-        
+        float angle = _startAngleDeg - (t * 360f);
+        line.localRotation = Quaternion.Euler(0f, 0f, angle);
     }
 }
