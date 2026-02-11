db9fd9eaf0883b73d8dc35f0ce85a2e7dbfb6cf6
ayato0913a <ayato0913a@gmail.com>
2026-02-11 09:59:19 +0900

    [ feat ] 円形ゲージ表示用のクラスを追加

--- Assets/Scripts/UI/RadialGaugeView.cs
+++ Assets/Scripts/UI/RadialGaugeView.cs
@@ -1,6 +1,18 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RadialGaugeView : MonoBehaviour
 {
-    
+    [SerializeField] private Image _fillImage;
+    [SerializeField] private Image _centerIconImage;
+
+    public void SetNormalized(float value)
+    {
+        _fillImage.fillAmount = Mathf.Clamp01(value);
+    }
+
+    public void SetCenterIcon(Sprite icon)
+    {
+        _centerIconImage.sprite = icon;
+    }
 }
