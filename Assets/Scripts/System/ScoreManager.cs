d0a5489666a4ed678cf97530c8f43955ec7684e2
ayato0913a <ayato0913a@gmail.com>
2026-02-13 20:35:42 +0900

    [ feat ] スコア管理クラス実装

--- Assets/Scripts/System/ScoreManager.cs
+++ Assets/Scripts/System/ScoreManager.cs
@@ -1,16 +1,51 @@
+using System;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static ScoreManager Instance { get; private set; }
+
+    public int Score => _score;
+
+    public event Action<int> OnScoreChanged;
+
+    public event Action<string,int> OnScoreAdded;
+
+    private int _score;
+
+    public void AddScore(int amount,string reason)
     {
-        
+        _score += amount;
+
+        OnScoreAdded?.Invoke(reason, amount);
+        OnScoreChanged?.Invoke(_score);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ResetScore()
     {
-        
+        _score = 0;
+        OnScoreChanged?.Invoke(_score);
+    }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void OnEnable()
+    {
+        GameEvents.OnScoreAdd += AddScore;
+    }
+
+    private void OnDisable()
+    {
+        GameEvents.OnScoreAdd -= AddScore;
     }
 }
