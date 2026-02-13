4b0c591e1f04a837e578eb9d3b135b58c3cbd0e0
ayato0913a <ayato0913a@gmail.com>
2026-02-13 21:49:46 +0900

    [ fix ] サマリ追加

--- Assets/Scripts/UI/ScorePouupSpawer.cs
+++ Assets/Scripts/UI/ScorePouupSpawer.cs
@@ -2,6 +2,9 @@ using DG.Tweening;
 using TMPro;
 using UnityEngine;
 
+/// <summary>
+///     スポーン地点からスコアポップアップを生成するクラス
+/// </summary>
 public class ScorePouupSpawer : MonoBehaviour
 {
     [SerializeField] private GameObject _popupPrefab;
@@ -10,8 +13,14 @@ public class ScorePouupSpawer : MonoBehaviour
     [SerializeField] private float _moveY;
     [SerializeField] private float _duration;
 
+    /// <summary>
+    ///     追加するスコアをポップアップで表示する
+    /// </summary>
+    /// <param name="reason"></param>
+    /// <param name="amount"></param>
     public void SpawnPopup(string reason,int amount)
     {
+        // ポップアップ生成
         GameObject popup = Instantiate(_popupPrefab, _spawnAnchor);
         RectTransform rect = popup.GetComponent<RectTransform>();
         TMP_Text text = popup.GetComponent<TMP_Text>();
@@ -29,12 +38,13 @@ public class ScorePouupSpawer : MonoBehaviour
 
         Sequence seq = DOTween.Sequence();
 
+        // フェードインと移動、フェードアウト
         seq.Append(text.DOFade(1f, 0.2f));
         seq.Join(rect.DOAnchorPos(endPos, _duration).SetEase(Ease.OutCubic));
         seq.Append(text.DOFade(0f, _duration))
            .SetUpdate(true);
 
-
+        // 終了時にポップアップを破棄
         seq.OnComplete(() => Destroy(popup));
     }
 
