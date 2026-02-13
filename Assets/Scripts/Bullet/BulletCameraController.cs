5051924948ee994913b64ea89595d0427a1bbbee
ayato0913a <ayato0913a@gmail.com>
2026-02-14 00:23:02 +0900

    [ feat ] 着弾して一定時間ホールドするように

--- Assets/Scripts/Bullet/BulletCameraController.cs
+++ Assets/Scripts/Bullet/BulletCameraController.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using Unity.Cinemachine;
 using UnityEngine;
 
@@ -7,8 +8,12 @@ public class BulletCameraController : MonoBehaviour
     [SerializeField] private CinemachineCamera _cinemachineCamera;
     [SerializeField] private GameObject _root;
 
+    [SerializeField] private float _holdSeconds;
+
     private bool _isShowing;
+    private bool _isHolding;
     private Transform _currentTarget;
+    private Tween _hideTween;
 
     public bool TryShow(Transform missile)
     {
@@ -27,11 +32,35 @@ public class BulletCameraController : MonoBehaviour
 
     public void Hide()
     {
+        _hideTween?.Kill();
+        _hideTween = null;
+
         _isShowing = false;
+        _isHolding = false;
         _currentTarget = null;
 
         _root.SetActive(false);
         _bulletCamera.enabled = false;
+    }
+
+    private void HideAfterHold()
+    {
+        if (_isHolding) return;
+        _isHolding = true;
+
+        _hideTween?.Kill();
+        _hideTween = DOVirtual.DelayedCall(_holdSeconds, () =>
+        {
+            _bulletCamera.enabled = false;
+
+            _cinemachineCamera.Follow = null;
+            _cinemachineCamera.LookAt = null;
+
+            _currentTarget = null;
+            _isHolding = false;
+            _isShowing = false;
+            _root.SetActive(false);
+        });
     }
 
     private void Awake()
@@ -41,7 +70,13 @@ public class BulletCameraController : MonoBehaviour
 
     private void LateUpdate()
     {
+        // 表示してないなら監視しない（ここが超重要）
+        if (!_isShowing) return;
+
+        // ターゲットが消えたら「ホールドして閉じる」
         if (_currentTarget == null || !_currentTarget.gameObject.activeInHierarchy)
-            Hide();
+        {
+            HideAfterHold();
+        }
     }
 }
