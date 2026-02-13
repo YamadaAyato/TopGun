9154dc205fad844e33883cd8fea17e21a7b176c6
ayato0913a <ayato0913a@gmail.com>
2026-02-13 23:44:28 +0900

    [ feat ] 弾の追従カメラ機能を追加

--- Assets/Scripts/Bullet/BulletCameraController.cs
+++ Assets/Scripts/Bullet/BulletCameraController.cs
@@ -1,16 +1,47 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class BulletCameraController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Camera _bulletCamera;
+    [SerializeField] private CinemachineCamera _cinemachineCamera;
+    [SerializeField] private GameObject _root;
+
+    private bool _isShowing;
+    private Transform _currentTarget;
+
+    public bool TryShow(Transform missile)
     {
-        
+        if (_isShowing) return false;
+
+        _currentTarget = missile;
+        _cinemachineCamera.Follow = _currentTarget;
+        _cinemachineCamera.LookAt = _currentTarget;
+
+        _root.SetActive(true);
+        _bulletCamera.enabled = true;
+
+        _isShowing = true;
+        return true;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Hide()
     {
-        
+        _isShowing = false;
+        _currentTarget = null;
+
+        _root.SetActive(false);
+        _bulletCamera.enabled = false;
+    }
+
+    private void Awake()
+    {
+        Hide();
+    }
+
+    private void LateUpdate()
+    {
+        if (_currentTarget == null || !_currentTarget.gameObject.activeInHierarchy)
+            Hide();
     }
 }
