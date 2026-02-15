9766ab343b51b0de882b4165d3782fa76779810b
ayato0913a <ayato0913a@gmail.com>
2026-02-15 22:42:46 +0900

    [ feat ] 入力感知してシーン遷移するクラス作成

--- Assets/Scripts/Title/SceneChangeFromTitle.cs
+++ Assets/Scripts/Title/SceneChangeFromTitle.cs
@@ -2,5 +2,26 @@ using UnityEngine;
 
 public class SceneChangeFromTitle : MonoBehaviour
 {
+    [SerializeField] private TitleInputHandler _titleInputHandler;
+    [SerializeField] private string _sceneNameToLoad = "";
 
+    private void SceneChenge()
+    {
+        if (_sceneNameToLoad == "")
+        {
+            Debug.LogError("Scene name is empty. Please set the scene name to load.");
+            return;
+        }
+        SceneLoader.LoadScene(_sceneNameToLoad);
+    }
+
+    private void OnEnable()
+    {
+        _titleInputHandler.OnClicked += SceneChenge;
+    }
+
+    private void OnDisable()
+    {
+        _titleInputHandler.OnClicked -= SceneChenge;
+    }
 }
