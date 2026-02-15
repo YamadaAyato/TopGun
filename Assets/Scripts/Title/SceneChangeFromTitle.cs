748efadbf78bc55307a8abd801fafb15bc829d56
ayato0913a <ayato0913a@gmail.com>
2026-02-16 05:01:03 +0900

    [ feat ] Fade機能追加

--- Assets/Scripts/Title/SceneChangeFromTitle.cs
+++ Assets/Scripts/Title/SceneChangeFromTitle.cs
@@ -1,7 +1,11 @@
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SceneChangeFromTitle : MonoBehaviour
 {
+    [SerializeField] private Image _fadeImage;
+    [SerializeField] private float _fadeDuration;
     [SerializeField] private TitleInputHandler _titleInputHandler;
     [SerializeField] private string _sceneNameToLoad = "";
 
@@ -12,12 +16,17 @@ public class SceneChangeFromTitle : MonoBehaviour
             Debug.LogError("Scene name is empty. Please set the scene name to load.");
             return;
         }
-        SceneLoader.LoadScene(_sceneNameToLoad);
+
+        _fadeImage.gameObject.SetActive(true);
+        _fadeImage.DOFade(1f, _fadeDuration)
+            .OnComplete(() => SceneLoader.LoadScene(_sceneNameToLoad));
     }
 
     private void OnEnable()
     {
         _titleInputHandler.OnClicked += SceneChenge;
+        _fadeImage.gameObject.SetActive(false);
+        _fadeImage.color = new Color(_fadeImage.color.r, _fadeImage.color.g, _fadeImage.color.b, 0f);
     }
 
     private void OnDisable()
