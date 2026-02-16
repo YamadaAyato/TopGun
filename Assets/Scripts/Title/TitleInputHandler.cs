9afda15860684115743bf545e10ed48dddc22577
ayato0913a <ayato0913a@gmail.com>
2026-02-16 10:02:09 +0900

    [ fix ] コメント追加

--- Assets/Scripts/Title/TitleInputHandler.cs
+++ Assets/Scripts/Title/TitleInputHandler.cs
@@ -2,11 +2,18 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using System;
 
+/// <summary>
+///     タイトルシーン内での入力を管理するクラス
+/// </summary>
 public class TitleInputHandler : MonoBehaviour
 {
     public event Action OnClicked;
     private RideActions _rideActions;
 
+    /// <summary>
+    ///     クリック時に呼び出される関数
+    /// </summary>
+    /// <param name="context"></param>
     private void HandleClick(InputAction.CallbackContext context)
     {
         OnClicked?.Invoke();
